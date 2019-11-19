8a7d9fa82ac2d7671e68538dd2b8d14cd0897afb
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-19 03:51:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,6 +85,8 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
+ 
+ programmer_hash
     
 end
 
@@ -108,6 +110,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
